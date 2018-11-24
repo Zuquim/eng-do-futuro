f5e9309faba8ab459dcffd9e49ef9dbce41a1204
Marco Túlio Zuquim <tulio@century.net.br>
2018-11-24 12:25:13 -0200

    Adding 'Nenhum resultado encontrado' in case of 0 results in searchbar

--- searchbar.js
+++ searchbar.js
@@ -7,12 +7,18 @@ function searchCards() {
     li = ul.getElementsByClassName('portfolio-card');
 
     // Loop through all list items, and hide those who don't match the search query
+    b = li.length;
     for (i = 0; i < li.length; i++) {
         a = li[i].getElementsByClassName("titulo")[0];
         if (a.innerHTML.toLowerCase().indexOf(filter) > -1) {
             li[i].style.display = "";
+            document.getElementById('aux').innerHTML = ""
         } else {
             li[i].style.display = "none";
+            b = b - 1;
         }
+    }
+    if (b == 0) {
+        document.getElementById('aux').innerHTML = "Nenhum resultado encontrado."
     }
 }
